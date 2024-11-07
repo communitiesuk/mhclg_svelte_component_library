3ac735f9a2ad15c4181a8432258d23db467fc402
Andrew Hillman <ajhillman_ddj@outlook.com>
2024-11-07 12:58:22 +0000

    Added workspace settings + added to-do list for a create a line chart exercise

--- src/routes/playground/create-a-reactive-line-chart/+page.svelte
+++ src/routes/playground/create-a-reactive-line-chart/+page.svelte
@@ -1,49 +1,52 @@
 <script>
+  let { data } = $props();
 
-    
-
-
+  $inspect(data);
 </script>
 
 <div class="chart-container">
-    <h5>We are going to build a reactive line chart</h5>
-    
-    <figure>
-            <figcaption>Steps:</figcaption>
-        <ul>
-            <li>1. Hello</li>
-            <li>2. Hello</li>
-        </ul>
-    </figure>
+  <h5>We are going to build a reactive line chart</h5>
+
+  <figure>
+    <figcaption>Steps:</figcaption>
+    <ul>
+      <li>1. Manipulate data</li>
+      <li>2. Create radio button component</li>
+      <li>
+        3. Add on-click functionalilty to radio buttons, that reactively filters
+        for the data we want to visualise
+      </li>
+      <li>4. Create the template for an svg line chart</li>
+      <li>
+        5. Pass the filtered data to our line chart, so that it updates whenever
+        a radio button is clicked
+      </li>
+    </ul>
+  </figure>
 </div>
 
-
-
-
 <style>
-
-.chart-container {
+  .chart-container {
     margin: 0px auto;
     width: 800px;
     display: flex;
     flex-direction: column;
     gap: 40px;
-}
+  }
 
-figure {
+  figure {
     display: flex;
     flex-direction: column;
     gap: 20px;
-}
+  }
 
-figcaption {
+  figcaption {
     font-weight: bold;
-}
+  }
 
-ul {
+  ul {
     display: flex;
     flex-direction: column;
     gap: 15px;
-}
-
+  }
 </style>

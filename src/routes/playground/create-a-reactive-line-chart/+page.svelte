50890d87a13d8a406c76c4088c46bb89b6523e89
Andrew Hillman <ajhillman_ddj@outlook.com>
2024-11-07 12:46:58 +0000

    Set up the playground for svelte practice exercises

---
+++ src/routes/playground/create-a-reactive-line-chart/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+
+    
+
+
+</script>
+
+<div class="chart-container">
+    <h5>We are going to build a reactive line chart</h5>
+    
+    <figure>
+            <figcaption>Steps:</figcaption>
+        <ul>
+            <li>1. Hello</li>
+            <li>2. Hello</li>
+        </ul>
+    </figure>
+</div>
+
+
+
+
+<style>
+
+.chart-container {
+    margin: 0px auto;
+    width: 800px;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+}
+
+figure {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+figcaption {
+    font-weight: bold;
+}
+
+ul {
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+}
+
+</style>

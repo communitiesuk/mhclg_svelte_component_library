f42c1ef8cf63122a0322810b2d97902847856978
Manbir.Jandu <manbir.jandu@levellingup.gov.uk>
2024-11-08 22:12:34 +0000

    Day1

--- src/routes/playground/create-a-reactive-line-chart/+page.svelte
+++ src/routes/playground/create-a-reactive-line-chart/+page.svelte
@@ -1,11 +1,76 @@
 <script>
   let { data } = $props();
 
-  $inspect(data);
+ $inspect(data.jsonData.chartData);
+
+  data.jsonData.chartData = data.jsonData.chartData.map((el) => ({
+    ...el,
+    yearInt: parseFloat(el.Year.slice(0, 4)),
+    }));
+
+console.log(data.jsonData.chartData);
+
+let metrics = [
+  ...new Set(
+    data.jsonData.chartData.map((d) => {
+      return d.Measure
+    })
+  ),
+];
+let areas = [...new Set(data.jsonData.chartData.map((el) => el.AreaCode))];
+
+console.log (areas);
+  
+  let manipulateData = metrics.map((metric) => ({
+  metric: metric,
+  lines: areas.map((area) => ({area: area, data: data.jsonData.chartData.filter((el) => el.AreaCode
+  === area && el.Measure === metric )}))
+  }));
+
+console.log (manipulateData);
+
+let areasNames = [
+  ...new Set(data.jsonData.chartData.map((el) => el.AreaNmae)),
+  ];
+  let metricNames = [
+   ...new Set(data.jsonData.chartData.map((el) => el.Measure)), 
+  ];
+let years = [...new Set(data.jsonData.chartData.map((el) => el.yearInt))];
+
+let selectedOption  = $state();
+$inspect(selectedOption);
 </script>
 
+ <div class="button-container">
+ <p>Area</p>
+<select>
+{#each areasNames as area}
+<option value={area}>{area}</option>
+{/each}
+</select>
+
+<p>Metric</p>
+<form>
+{#each metricNames as option}
+<input type="radio" name="metrics-select" bind:group={selectedOption} />
+<label>
+{option}
+</label>
+<br />
+{/each}
+</form>
+<p>{selectedOption}</p>
+
+</div>
+
+
+
+
+
+<!-- </script> -->
+
 <div class="chart-container">
-  <h5>We are going to build a reactive line chart</h5>
+  <h5>We are going to build a reactive line chart</h5>/
 
   <figure>
     <figcaption>Steps:</figcaption>
@@ -24,6 +89,35 @@
     </ul>
   </figure>
 </div>
+
+<!--LABEL plus Rdio buttons -->
+ <!--
+<div class="radio-container">
+  
+  <label>Choose a metric:</label>
+  
+ 
+  <div>
+  <input type="radio" id="recycle-rate" name="metric-selection" value="Household waste recycling rate" checked 
+  />
+      <label for="recycle-rate">Household recycling rate</label>
+  </div>
+  
+  <div>
+  <input type="radio" id="contamination-rate" name="metric-selection" value="Recycling contamination rate" />
+      <label for="contamination-rate">Recycling contamination rate</label>
+  </div>
+  
+    <div>
+   <input type="radio" id="residual-household-waste" name="metric-selection" value="Residual household waste" />
+      <label for="residual-household-waste">Residual household waste</label>
+  </div>
+ -->
+
+
+
+
+<!-- ****************** CSS **************-->
 
 <style>
   .chart-container {
@@ -49,4 +143,23 @@
     flex-direction: column;
     gap: 15px;
   }
+  
+label {
+  margin-right: 5px;
+}
+
+option {
+  font-style: italic;
+}
+
+.radio-container {
+  padding: 10px;
+  border-color: #cc7400;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+
 </style>
+

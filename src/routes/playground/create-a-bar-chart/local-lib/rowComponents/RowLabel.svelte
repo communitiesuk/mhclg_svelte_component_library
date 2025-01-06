289a9e9c12cf763b39ae79197b8d4348797e5ffc
Duncan Bradley <duncan.bradley@levellingup.gov.uk>
2025-01-06 14:09:47 +0000

    add basic labels

---
+++ src/routes/playground/create-a-bar-chart/local-lib/rowComponents/RowLabel.svelte
@@ -0,0 +1,8 @@
+<script>
+  let { row } = $props();
+  $inspect(row);
+</script>
+
+<g transform="translate({-30},{0})">
+  <text x={-70} y={10}>{row.areaCode}</text>
+</g>
---
+++ src/routes/playground/create-a-bar-chart/local-lib/rowComponents/RowValue.svelte
@@ -0,0 +1,10 @@
+<script>
+  let { row, xFunction } = $props();
+  let offsetX = $derived(xFunction(row.y) + 10); // Reactive declaration
+$inspect(offsetX)
+
+</script>
+
+<g transform="translate({offsetX},{0})">
+    <text x={0} y={10}>{row.y}</text>
+  </g>

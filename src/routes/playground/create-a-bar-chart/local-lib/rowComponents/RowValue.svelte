24db0bd5ac904b15537dcc898338b969fcdc1f28
Duncan Bradley <duncan.bradley@levellingup.gov.uk>
2025-01-23 11:35:33 +0000

    resposition label for negative bars

--- src/routes/playground/create-a-bar-chart/local-lib/rowComponents/RowValue.svelte
+++ src/routes/playground/create-a-bar-chart/local-lib/rowComponents/RowValue.svelte
@@ -1,9 +1,9 @@
 <script>
   let { row, xFunction, barHeight } = $props();
-  let offsetX = $derived(xFunction(row.y) + 10); // Reactive declaration
+  let offsetX = $derived(xFunction(row.y));
   $inspect(offsetX);
 </script>
 
 <g transform="translate({offsetX},{barHeight/2})">
-  <text dominant-baseline="central" x={0} y={0}>{row.y}</text>
+  <text dominant-baseline="central" x={0} y={0} text-anchor={row.y > 0 ? "start" : "end"}>{row.y}</text>
 </g>

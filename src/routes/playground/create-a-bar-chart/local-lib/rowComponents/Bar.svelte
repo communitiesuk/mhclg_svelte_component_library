5b375830f72987e711efc278dc53546d9594394d
Duncan Bradley <duncan.bradley@levellingup.gov.uk>
2025-01-23 14:17:48 +0000

    use default colours from dataset

--- src/routes/playground/create-a-bar-chart/local-lib/rowComponents/Bar.svelte
+++ src/routes/playground/create-a-bar-chart/local-lib/rowComponents/Bar.svelte
@@ -6,5 +6,7 @@
   width={Math.abs(xFunction(row.y) - xFunction(0))}
   height={barHeight}
   x={row.y < 0 ? xFunction(row.y) : xFunction(0)}
-  fill={row.y < 0 & colours.colourNegativeBars ? colours.negativeBarColour : "#bababa"}
+  fill={(row.y < 0) & colours.colourNegativeBars
+    ? colours.negativeBarColour
+    : row.colour}
 />

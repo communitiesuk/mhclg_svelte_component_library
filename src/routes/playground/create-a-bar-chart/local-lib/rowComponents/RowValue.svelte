b7ba5095ba21861d43154dadf4f9f82e24553256
Duncan Bradley <duncan.bradley@levellingup.gov.uk>
2025-01-28 09:52:16 +0000

    dynamic positioning of value labels inside/outside bar

--- src/routes/playground/create-a-bar-chart/local-lib/rowComponents/RowValue.svelte
+++ src/routes/playground/create-a-bar-chart/local-lib/rowComponents/RowValue.svelte
@@ -1,8 +1,24 @@
 <script>
-  let { row, xFunction, barHeight } = $props();
+  let { row, xFunction, barHeight, colours, chartWidth } = $props();
   let offsetX = $derived(xFunction(row.y));
+  let minSpaceOutside = $derived(chartWidth - 70)
 </script>
 
-<g transform="translate({offsetX},{barHeight/2})">
-  <text dominant-baseline="central" x={0} y={0} text-anchor={row.y > 0 ? "start" : "end"}>{row.y}</text>
-</g>
+{#snippet rowValueLabel(x, textAnchor)}
+<g transform="translate({offsetX},{barHeight / 2})">
+  <text
+    dominant-baseline="central"
+    x={x}
+    y={0}
+    text-anchor={textAnchor}>{row.y}</text></g>
+{/snippet}
+
+{#if row.y < 0 && xFunction(Math.abs(row.y)) > minSpaceOutside}
+{@render rowValueLabel(5, 'start')}
+{:else if row.y < 0 && xFunction(Math.abs(row.y)) < minSpaceOutside}
+{@render rowValueLabel(-5, 'end')}
+{:else if row.y > 0 && xFunction(Math.abs(row.y)) > minSpaceOutside}
+{@render rowValueLabel(-5, 'end')}
+{:else}
+{@render rowValueLabel(5, 'start')}
+{/if}

607306903c8554ffc99899387dcd531e9994ba6f
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-02-12 18:27:59 +0000

    Change placeholder text

--- src/lib/components/ui/Details.svelte
+++ src/lib/components/ui/Details.svelte
@@ -5,8 +5,7 @@
 <details style="color: {textColor}">
   <summary><span>Summary text</span></summary>
   <div>
-    This is information that only certain users need to see. For this reason we
-    have hidden from view to make scanning the page easier for the majority of
-    users.
+    This is information that only certain users need to see. It is hidden from
+    view by default to make scanning the page easier for the majority of users.
   </div>
 </details>

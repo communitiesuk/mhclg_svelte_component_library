e89246817fbcf5f6fab60046b7757c9534bd82ed
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-02-13 15:08:19 +0000

    Add text content parameters

--- src/routes/components/ui/details/+page.svelte
+++ src/routes/components/ui/details/+page.svelte
@@ -55,6 +55,21 @@
     homepage ??
     addIndexAndInitalValue([
       {
+        name: "summaryText",
+        category: "content",
+        isProp: true,
+        inputType: "input",
+        value: "Summary text",
+      },
+      {
+        name: "detailedText",
+        category: "content",
+        isProp: true,
+        inputType: "input",
+        value:
+          "This is information that only certain users need to see. It is hidden from view by default to make scanning the page easier for the majority of users who do not need to see it.",
+      },
+      {
         name: "textColor",
         category: "styling",
         isProp: true,

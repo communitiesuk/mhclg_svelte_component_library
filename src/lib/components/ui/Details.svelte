6bdbe51eb869ca63633074854aaebe09c6a65ef0
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-02-13 12:13:12 +0000

    Add gds styling by adding classes to tags

--- src/lib/components/ui/Details.svelte
+++ src/lib/components/ui/Details.svelte
@@ -2,10 +2,13 @@
   let { textColor } = $props();
 </script>
 
-<details style="color: {textColor}">
-  <summary><span>Summary text</span></summary>
-  <div>
+<details class="govuk-details" style="color: {textColor}">
+  <summary class="govuk-details__summary">
+    <span class="govuk-details__summary-text">Summary text</span>
+  </summary>
+  <div class="govuk-details__text">
     This is information that only certain users need to see. It is hidden from
-    view by default to make scanning the page easier for the majority of users.
+    view by default to make scanning the page easier for the majority of users
+    who do not need to see it.
   </div>
 </details>

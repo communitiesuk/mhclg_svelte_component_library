00db6c467190e249e7e9ecbf81ac380e02d58709
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-02-14 11:33:42 +0000

    Create component and component page

---
+++ src/lib/components/ui/Button.svelte
@@ -0,0 +1,7 @@
+<script>
+  let { textContent, bgColor, textColor, size } = $props();
+</script>
+
+<button type="submit" class="govuk-button" data-module="govuk-button">
+  {textContent}
+</button>

3d7f3d023557349555dc5f84c1110c80b045a353
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-02-25 14:41:52 +0000

    Create route and component files

---
+++ src/lib/components/data-vis/table/Table.svelte
@@ -0,0 +1,19 @@
+<table>
+  <caption></caption>
+  <thead>
+    <tr>
+      <th>column 1</th>
+      <th>column 2</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>data cell</td>
+      <td>data cell</td>
+    </tr>
+    <tr>
+      <td>data cell</td>
+      <td>data cell</td>
+    </tr>
+  </tbody>
+</table>

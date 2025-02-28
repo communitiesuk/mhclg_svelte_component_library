92cefcb352b7d17bf698311a06d5114ddc52606d
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-02-28 13:24:40 +0000

    Add gds styling

--- src/lib/components/data-vis/table/Table.svelte
+++ src/lib/components/data-vis/table/Table.svelte
@@ -1,19 +1,23 @@
-<table>
-  <caption></caption>
-  <thead>
-    <tr>
-      <th>column 1</th>
-      <th>column 2</th>
+<table class="govuk-table">
+  <caption class="govuk-table__caption govuk-table__caption--m"
+    >Recycling rates</caption
+  >
+  <thead class="govuk-table__head">
+    <tr class="govuk-table__row">
+      <th scope="col" class="govuk-table__header">Local authority</th>
+      <th scope="col" class="govuk-table__header govuk-table__header--numeric"
+        >Recycling rate</th
+      >
     </tr>
   </thead>
-  <tbody>
-    <tr>
-      <td>data cell</td>
-      <td>data cell</td>
+  <tbody class="govuk-table__body">
+    <tr class="govuk-table__row">
+      <td class="govuk-table__cell">Stockport</td>
+      <td class="govuk-table__cell govuk-table__cell--numeric">56.5%</td>
     </tr>
-    <tr>
-      <td>data cell</td>
-      <td>data cell</td>
+    <tr class="govuk-table__row">
+      <td class="govuk-table__cell">Leeds</td>
+      <td class="govuk-table__cell govuk-table__cell--numeric">80%</td>
     </tr>
   </tbody>
 </table>

f66b5c64912cac91972820c69bd9a095c8e437d8
Duncan Bradley <duncan.bradley@levellingup.gov.uk>
2025-03-18 15:43:54 +0000

    example with few lines and multiple colours

--- src/lib/components/data-vis/line-chart/Lines.svelte
+++ src/lib/components/data-vis/line-chart/Lines.svelte
@@ -10,36 +10,60 @@
     xFunction,
     yFunction,
   } = $props();
+
+  let showAllData = $state(false);
+
+  let colors = ["red", "blue", "green", "orange", "purple", "cyan"];
 </script>
 
-{#each data.lines as line, i}
-  {#if line.areaCode !== selectedAreaCode}
+{#if showAllData}
+  {#each data.lines as line, i}
+    {#if line.areaCode !== selectedAreaCode}
+      <Line
+        {lineFunction}
+        dataArray={line.data}
+        pathStrokeColor="black"
+        pathStrokeWidth="1"
+        opacity="0.15"
+        dataId={line.areaCode}
+        onMouseMove={() => {
+          selectedAreaCode = line.areaCode;
+        }}
+      ></Line>
+    {/if}
+  {/each}
+  <Line
+    {lineFunction}
+    dataArray={data.lines.find((el) => el.areaCode === selectedAreaCode).data}
+    pathStrokeColor="red"
+    pathStrokeWidth="5"
+    opacity="1"
+    includeMarkers={true}
+    markerRadius="8"
+    markerStroke="red"
+    markerFill="white"
+    {xFunction}
+    {yFunction}
+  ></Line>
+{:else}
+  {#each data.lines.slice(0, 5) as line, i}
     <Line
       {lineFunction}
       dataArray={line.data}
-      pathStrokeColor="black"
-      pathStrokeWidth="1"
-      opacity="0.15"
+      pathStrokeColor={colors[i]}
+      pathStrokeWidth="5"
+      opacity="1"
+      includeMarkers={false}
+      markerRadius="8"
+      markerStroke="red"
+      markerFill="white"
       dataId={line.areaCode}
       onMouseMove={() => {
         selectedAreaCode = line.areaCode;
       }}
     ></Line>
-  {/if}
-{/each}
-<Line
-  {lineFunction}
-  dataArray={data.lines.find((el) => el.areaCode === selectedAreaCode).data}
-  pathStrokeColor="red"
-  pathStrokeWidth="5"
-  opacity="1"
-  includeMarkers={true}
-  markerRadius="8"
-  markerStroke="red"
-  markerFill="white"
-  {xFunction}
-  {yFunction}
-></Line>
+  {/each}
+{/if}
 <g>
   <CategoryLabel
     {chartWidth}

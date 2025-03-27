a0d9d35ba6da06f3f4e33513d18c99dc15e20bc1
Andrew Hillman <63984614+ajhillman-ddj@users.noreply.github.com>
2025-03-27 15:26:58 +0000

    Combine ticks text and path into single group

--- src/routes/playground/create-a-bar-chart/local-lib/rowComponents/Ticks.svelte
+++ src/routes/playground/create-a-bar-chart/local-lib/rowComponents/Ticks.svelte
@@ -3,19 +3,16 @@
   $inspect(ticksArray);
 </script>
 
-{#each ticksArray as tick, i}
-  <path
-    transform="translate({yFunction(tick)}, 0)"
-    d="M0 0 l0 -8"
-    stroke="black"
-    stroke-width="2px"
-  ></path>
-  <text
-    transform="translate({yFunction(tick)}, -10)"
-    font-size="14"
-    text-anchor="middle"
-    fill="black"
-  >
-    {tick}
-  </text>
+{#each ticksArray as tick}
+  <g transform="translate({yFunction(tick)}, 0)">
+    <path d="M0 0 l0 -8" stroke="black" stroke-width="2px"></path>
+    <text
+      transform="translate(0, -10)"
+      font-size="14"
+      text-anchor="middle"
+      fill="black"
+    >
+      {tick}
+    </text>
+  </g>
 {/each}

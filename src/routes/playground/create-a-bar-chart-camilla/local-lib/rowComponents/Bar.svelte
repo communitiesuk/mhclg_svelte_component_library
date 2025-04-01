9c33869c052bc5a571315e4fda411630b7b410b0
Andrew Hillman <63984614+ajhillman-ddj@users.noreply.github.com>
2025-04-01 11:02:21 +0100

    Added Camilla's bar chart manually

---
+++ src/routes/playground/create-a-bar-chart-camilla/local-lib/rowComponents/Bar.svelte
@@ -0,0 +1,23 @@
+<script>
+  let { row, yFunction, rowHeight } = $props();
+</script>
+
+{#if isNaN(row.y)}
+  <line
+    stroke-width="{rowHeight - 5}px"
+    x1={yFunction(0)}
+    x2={yFunction(0)}
+    y1="0"
+    y2="0"
+    stroke={row.color ?? "black"}
+  ></line>
+{:else}
+  <line
+    stroke-width="{rowHeight - 5}px"
+    x1={yFunction(0)}
+    x2={yFunction(row.y)}
+    y1="0"
+    y2="0"
+    stroke={row.color ?? "black"}
+  ></line>
+{/if}

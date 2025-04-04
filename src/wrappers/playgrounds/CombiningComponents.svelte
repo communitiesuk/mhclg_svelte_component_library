310650134b03184e1b70b306e7355200c2166a3e
Andrew Hillman <63984614+ajhillman-ddj@users.noreply.github.com>
2025-04-04 09:56:21 +0100

    Minor edits

--- src/wrappers/playgrounds/CombiningComponents.svelte
+++ src/wrappers/playgrounds/CombiningComponents.svelte
@@ -43,6 +43,7 @@
   let selectedIncludes = $state([]);
 
   let navigationItems = [
+    { href: "", label: "Get started", isActive: true },
     { href: "#line-chart", label: "Line Chart" },
     { href: "#map", label: "Map" },
   ];
@@ -51,11 +52,7 @@
 <div class="chart-container">
   <div class="mt-20">
     <Header fullWidthBorder={true} />
-    <ServiceNavigation
-      serviceName="Sprint 24 - Show and Tell"
-      serviceUrl="/my-service"
-      {navigationItems}
-    />
+    <ServiceNavigation serviceName="" serviceUrl="" {navigationItems} />
     <div class="top-level-container p-2 mx-auto mt-4" style="width: 960px;">
       <Breadcrumbs
         items={[
@@ -123,6 +120,7 @@
         </div>
       {:else}
         <div style="width: 722px;" class="mb-20">
+          <h4 class="mt-12 mb-8">Heading 1</h4>
           <p class="mb-8">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -139,16 +137,6 @@
             dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
             aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
             eos qui ratione voluptatem sequi nesciunt.
-          </p>
-          <p class="mb-8">
-            Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-            consectetur, adipisci velit, sed quia non numquam eius modi tempora
-            incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut
-            enim ad minima veniam, quis nostrum exercitationem ullam corporis
-            suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis
-            autem vel eum iure reprehenderit qui in ea voluptate velit esse quam
-            nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-            voluptas nulla pariatur?
           </p>
         </div>
       {/if}

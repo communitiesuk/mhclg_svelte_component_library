0f2c485bdbc3c23c24876e11a27245e8bf433e30
Ibrahim Haizel <ibrahim.haizel@communities.gov.uk>
2025-04-09 14:18:53 +0100

    Update tab content to improve clarity and organisation, replacing HTML strings with Svelte snippets for better maintainability.

--- src/wrappers/components/ui/tabs/Examples.svelte
+++ src/wrappers/components/ui/tabs/Examples.svelte
@@ -5,21 +5,23 @@
   import * as codeBlocks from "./codeBlocks.js";
 
   import Tabs from "$lib/components/ui/Tabs.svelte";
+  import Radios from "$lib/components/ui/Radios.svelte";
+  import Button from "$lib/components/ui/Button.svelte";
 
   let accordionSnippetSections = [
     {
       id: "1",
-      heading: "1. Basic Tabs Example",
+      heading: "1. Basic Tabs Example with String Content",
       content: Example1,
     },
     {
       id: "2",
-      heading: "2. Pre-selected Tab Example",
+      heading: "2. Using Snippets with Pre-selected Tab",
       content: Example2,
     },
     {
       id: "3",
-      heading: "3. Tabs with Rich Content",
+      heading: "3. Complex Content Using Svelte Snippets",
       content: Example3,
     },
   ];
@@ -45,6 +47,124 @@
   </Accordion>
 </div>
 
+{#snippet tab1Content()}
+  <h2 class="govuk-heading-l">First Tab Content</h2>
+  <p class="govuk-body">This is the content for the first tab.</p>
+{/snippet}
+
+{#snippet tab2Content()}
+  <h2 class="govuk-heading-l">Second Tab Content</h2>
+  <p class="govuk-body">This is the content for the second tab.</p>
+{/snippet}
+
+{#snippet tab3Content()}
+  <h2 class="govuk-heading-l">Third Tab Content</h2>
+  <p class="govuk-body">This is the content for the third tab.</p>
+{/snippet}
+
+{#snippet example1Tab1Content()}
+  <h2 class="govuk-heading-l">First Tab Content</h2>
+  <p class="govuk-body">This tab is not selected by default.</p>
+{/snippet}
+
+{#snippet example1Tab2Content()}
+  <h2 class="govuk-heading-l">Second Tab Content</h2>
+  <p class="govuk-body">
+    This tab is selected by default because it has selected=true.
+  </p>
+{/snippet}
+
+{#snippet example1Tab3Content()}
+  <h2 class="govuk-heading-l">Third Tab Content</h2>
+  <p class="govuk-body">This tab is not selected by default.</p>
+{/snippet}
+
+{#snippet chartsTabContent()}
+  <h2 class="govuk-heading-l">Charts & Data</h2>
+  <div class="govuk-grid-row">
+    <div class="govuk-grid-column-one-half">
+      <table class="govuk-table">
+        <caption class="govuk-table__caption govuk-table__caption--m"
+          >Monthly statistics</caption
+        >
+        <thead class="govuk-table__head">
+          <tr class="govuk-table__row">
+            <th scope="col" class="govuk-table__header">Month</th>
+            <th scope="col" class="govuk-table__header">Value</th>
+          </tr>
+        </thead>
+        <tbody class="govuk-table__body">
+          <tr class="govuk-table__row">
+            <td class="govuk-table__cell">January</td>
+            <td class="govuk-table__cell">125</td>
+          </tr>
+          <tr class="govuk-table__row">
+            <td class="govuk-table__cell">February</td>
+            <td class="govuk-table__cell">163</td>
+          </tr>
+          <tr class="govuk-table__row">
+            <td class="govuk-table__cell">March</td>
+            <td class="govuk-table__cell">142</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="govuk-grid-column-one-half">
+      <div
+        style="height: 200px; background-color: #f3f2f1; display: flex; align-items: center; justify-content: center;"
+      >
+        <p class="govuk-body">Chart Placeholder</p>
+      </div>
+    </div>
+  </div>
+{/snippet}
+
+{#snippet formsTabContent()}
+  <h2 class="govuk-heading-l">Forms & Input</h2>
+  <form class="govuk-form-group">
+    <label class="govuk-label" for="example-input">Example input</label>
+    <input
+      class="govuk-input govuk-input--width-20"
+      id="example-input"
+      name="example-input"
+      type="text"
+    />
+
+    <div class="govuk-form-group" style="margin-top: 20px;">
+      <Radios
+        name="example-radios"
+        legend="Select an option"
+        options={[
+          { value: "yes", label: "Yes" },
+          { value: "no", label: "No" },
+        ]}
+      />
+    </div>
+
+    <div style="margin-top: 20px;">
+      <Button textContent="Continue" buttonType="default" />
+    </div>
+  </form>
+{/snippet}
+
+{#snippet mediaTabContent()}
+  <h2 class="govuk-heading-l">Media Content</h2>
+  <figure class="govuk-!-margin-bottom-6">
+    <img
+      src="https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d"
+      alt="Person working on laptop with notebook and coffee"
+      style="max-width: 100%; height: auto;"
+    />
+    <figcaption class="govuk-body-s govuk-!-margin-top-2">
+      Modern workspace with laptop and notebook
+    </figcaption>
+  </figure>
+  <p class="govuk-body">
+    Tabs can contain various types of media including images, videos, and
+    interactive content.
+  </p>
+{/snippet}
+
 {#snippet Example1()}
   <div class="p-5 bg-white">
     <Tabs
@@ -53,20 +173,20 @@
         {
           id: "tab-1",
           label: "First Tab",
-          content: `<h2 class="govuk-heading-l">First Tab Content</h2>
-            <p class="govuk-body">This is the content for the first tab.</p>`,
+          content:
+            "Welcome to the first tab! This is a simple example showing how tabs can display basic text content.",
         },
         {
           id: "tab-2",
           label: "Second Tab",
-          content: `<h2 class="govuk-heading-l">Second Tab Content</h2>
-            <p class="govuk-body">This is the content for the second tab.</p>`,
+          content:
+            "Here's the second tab with some different content. Tabs are great for organizing related information.",
         },
         {
           id: "tab-3",
           label: "Third Tab",
-          content: `<h2 class="govuk-heading-l">Third Tab Content</h2>
-            <p class="govuk-body">This is the content for the third tab.</p>`,
+          content:
+            "And finally, the third tab. You can navigate between tabs using either your mouse or keyboard.",
         },
       ]}
     />
@@ -82,21 +202,18 @@
         {
           id: "example-tab-1",
           label: "First Tab",
-          content: `<h2 class="govuk-heading-l">First Tab Content</h2>
-            <p class="govuk-body">This tab is not selected by default.</p>`,
+          content: example1Tab1Content,
         },
         {
           id: "example-tab-2",
           label: "Second Tab",
           selected: true,
-          content: `<h2 class="govuk-heading-l">Second Tab Content</h2>
-            <p class="govuk-body">This tab is selected by default because it has selected=true.</p>`,
+          content: example1Tab2Content,
         },
         {
           id: "example-tab-3",
           label: "Third Tab",
-          content: `<h2 class="govuk-heading-l">Third Tab Content</h2>
-            <p class="govuk-body">This tab is not selected by default.</p>`,
+          content: example1Tab3Content,
         },
       ]}
     />
@@ -112,76 +229,17 @@
         {
           id: "rich-tab-1",
           label: "Charts & Data",
-          content: `<h2 class="govuk-heading-l">Charts & Data</h2>
-            <div class="govuk-grid-row">
-              <div class="govuk-grid-column-one-half">
-                <table class="govuk-table">
-                  <caption class="govuk-table__caption govuk-table__caption--m">Monthly statistics</caption>
-                  <thead class="govuk-table__head">
-                    <tr class="govuk-table__row">
-                      <th scope="col" class="govuk-table__header">Month</th>
-                      <th scope="col" class="govuk-table__header">Value</th>
-                    </tr>
-                  </thead>
-                  <tbody class="govuk-table__body">
-                    <tr class="govuk-table__row">
-                      <td class="govuk-table__cell">January</td>
-                      <td class="govuk-table__cell">125</td>
-                    </tr>
-                    <tr class="govuk-table__row">
-                      <td class="govuk-table__cell">February</td>
-                      <td class="govuk-table__cell">163</td>
-                    </tr>
-                    <tr class="govuk-table__row">
-                      <td class="govuk-table__cell">March</td>
-                      <td class="govuk-table__cell">142</td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-              <div class="govuk-grid-column-one-half">
-                <div style="height: 200px; background-color: #f3f2f1; display: flex; align-items: center; justify-content: center;">
-                  <p class="govuk-body">Chart Placeholder</p>
-                </div>
-              </div>
-            </div>`,
+          content: chartsTabContent,
         },
         {
           id: "rich-tab-2",
           label: "Forms & Input",
-          content: `<h2 class="govuk-heading-l">Forms & Input</h2>
-            <form class="govuk-form-group">
-              <label class="govuk-label" for="example-input">Example input</label>
-              <input class="govuk-input govuk-input--width-20" id="example-input" name="example-input" type="text">
-              
-              <div class="govuk-form-group" style="margin-top: 20px;">
-                <fieldset class="govuk-fieldset">
-                  <legend class="govuk-fieldset__legend">Select an option</legend>
-                  <div class="govuk-radios">
-                    <div class="govuk-radios__item">
-                      <input class="govuk-radios__input" id="radio-1" name="radio" type="radio" value="yes">
-                      <label class="govuk-label govuk-radios__label" for="radio-1">Yes</label>
-                    </div>
-                    <div class="govuk-radios__item">
-                      <input class="govuk-radios__input" id="radio-2" name="radio" type="radio" value="no">
-                      <label class="govuk-label govuk-radios__label" for="radio-2">No</label>
-                    </div>
-                  </div>
-                </fieldset>
-              </div>
-              
-              <button type="button" class="govuk-button" style="margin-top: 20px;">Continue</button>
-            </form>`,
+          content: formsTabContent,
         },
         {
           id: "rich-tab-3",
           label: "Media Content",
-          content: `<h2 class="govuk-heading-l">Media Content</h2>
-            <figure class="govuk-!-margin-bottom-6">
-              <img src="https://via.placeholder.com/800x400" alt="Example image" style="max-width: 100%; height: auto;">
-              <figcaption class="govuk-body-s govuk-!-margin-top-2">Example image caption</figcaption>
-            </figure>
-            <p class="govuk-body">Tabs can contain various types of media including images, videos, and interactive content.</p>`,
+          content: mediaTabContent,
         },
       ]}
     />

efbf0d0c71a5b874736e75a04155d49f3289a8d5
Ibrahim Haizel <ibrahim.haizel@communities.gov.uk>
2025-04-11 16:26:44 +0100

    Enhance NotificationBanner examples: add multiple code snippets demonstrating various use cases, including neutral and outcome banners with string and snippet content, using Svelte 5 syntax.

--- src/wrappers/components/ui/notification-banner/Examples.svelte
+++ src/wrappers/components/ui/notification-banner/Examples.svelte
@@ -4,13 +4,38 @@
   import CodeBlock from "$lib/package-wrapping/CodeBlock.svelte";
   import * as codeBlocks from "./codeBlocks.js";
 
-import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
+  import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
 
   let accordionSnippetSections = [
     {
       id: "1",
-      heading: "1. Example 1 - describe the use case here",
+      heading: "1. Neutral banner with string content",
       content: Example1,
+    },
+    {
+      id: "2",
+      heading: "2. Neutral banner with string content and link",
+      content: Example2,
+    },
+    {
+      id: "3",
+      heading: "3. Outcome (success) banner with string content",
+      content: Example3,
+    },
+    {
+      id: "4",
+      heading: "4. Outcome banner with heading and body content",
+      content: Example4,
+    },
+    {
+      id: "5",
+      heading: "5. Neutral banner with snippet content",
+      content: Example5,
+    },
+    {
+      id: "6",
+      heading: "6. Neutral banner with custom H3 title",
+      content: Example6,
     },
   ];
 </script>
@@ -37,32 +62,70 @@ import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
 
 {#snippet Example1()}
   <div class="p-5 bg-white">
-    <Template
-      componentNameProp="Example 1"
-      checkboxProp={true}
-      dropdownProp="Dragonfruit"
-      jsObjectProp={[
-        {
-          name: "Borussia Dortmund",
-          country: "Germany",
-          color: "#fdff7d",
-        },
-        { name: "Liverpool FC", country: "UK", color: "#f59fad" },
-        {
-          name: "SSC Napoli",
-          country: "Italy",
-          color: "#69bfff",
-        },
-        {
-          name: "S.L. Benfica",
-          country: "Portugal",
-          color: "#ff8c96",
-        },
-      ]}
-      functionProp={function () {
-        window.alert(`Example 1 functionProp has been triggered.`);
-      }}
-    ></Template>
+    <NotificationBanner
+      type="neutral"
+      content="There may be a delay in processing your application because of the coronavirus outbreak."
+    />
   </div>
   <CodeBlock code={codeBlocks.codeBlock1} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example2()}
+  <div class="p-5 bg-white">
+    <NotificationBanner
+      type="neutral"
+      content="You have 7 days left to send your application."
+      linkText="View application"
+      linkHref="#"
+    />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock2} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example3()}
+  <div class="p-5 bg-white">
+    <NotificationBanner
+      type="outcome"
+      content="Your details have been updated."
+    />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock3} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example4()}
+  <div class="p-5 bg-white">
+    <NotificationBanner
+      type="outcome"
+      additionalContentHeading="Training outcome recorded and trainee withdrawn"
+      content="Contact example@department.gov.uk if you think there's a problem."
+    />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock4} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet ContentSnippet()}
+  <p class="govuk-notification-banner__heading">Service update</p>
+  <p class="govuk-body">
+    We are experiencing high demand. Please check back later if you cannot
+    access the service.
+  </p>
+{/snippet}
+
+{#snippet Example5()}
+  <div class="p-5 bg-white">
+    <NotificationBanner type="neutral" content={ContentSnippet} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock5} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example6()}
+  <div class="p-5 bg-white">
+    <NotificationBanner
+      type="neutral"
+      title="Service Information"
+      titleHeadingLevel="h3"
+      content="This service will be unavailable on Sunday 14th July between 8am and 1pm."
+    />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock6} language="svelte"></CodeBlock>
 {/snippet}
--- src/wrappers/components/ui/notification-banner/codeBlocks.js
+++ src/wrappers/components/ui/notification-banner/codeBlocks.js
@@ -1,32 +1,75 @@
 export const codeBlock1 = `
 <script>
-
-  import Template from "$lib/package-wrapping/Template.svelte";
-
+  import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
 </script>
 
-<Template
-checkboxProp={true}
-dropdownProp="Dragonfruit"
-jsObjectProp={[
-  {
-    name: "Borussia Dortmund",
-    country: "Germany",
-    color: "#fdff7d",
-  },
-  { name: "Liverpool FC", country: "UK", color: "#f59fad" },
-  {
-    name: "SSC Napoli",
-    country: "Italy",
-    color: "#69bfff",
-  },
-  {
-    name: "S.L. Benfica",
-    country: "Portugal",
-    color: "#ff8c96",
-  },
-]}
-functionProp={function (event, pokemon) {
-  window.alert("Example 1 functionProp has been triggered.");
-}}
-></Template>`;
+<NotificationBanner
+  type="neutral"
+  content="There may be a delay in processing your application because of the coronavirus outbreak."
+/>
+`;
+
+export const codeBlock2 = `
+<script>
+  import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
+</script>
+
+<NotificationBanner
+  type="neutral"
+  content="You have 7 days left to send your application."
+  linkText="View application"
+  linkHref="#"
+/>
+`;
+
+export const codeBlock3 = `
+<script>
+  import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
+</script>
+
+<NotificationBanner
+  type="outcome"
+  content="Your details have been updated."
+/>
+`;
+
+export const codeBlock4 = `
+<script>
+  import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
+</script>
+
+<NotificationBanner
+  type="outcome"
+  additionalContentHeading="Training outcome recorded and trainee withdrawn"
+  content="Contact example@department.gov.uk if you think there's a problem."
+/>
+`;
+
+export const codeBlock5 = `
+<script>
+  import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
+
+  {#snippet ContentSnippet()}
+    <p class="govuk-notification-banner__heading">Service update</p>
+    <p class="govuk-body">
+      We are experiencing high demand. Please check back later if you cannot access
+      the service.
+    </p>
+  {/snippet}
+</script>
+
+<NotificationBanner type="neutral" content={ContentSnippet} />
+`;
+
+export const codeBlock6 = `
+<script>
+  import NotificationBanner from "$lib/components/ui/NotificationBanner.svelte";
+</script>
+
+<NotificationBanner
+  type="neutral"
+  title="Service Information"
+  titleHeadingLevel="h3"
+  content="This service will be unavailable on Sunday 14th July between 8am and 1pm."
+/>
+`;

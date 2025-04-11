8223341b4736dd0c3af2bb4283b8453ab85c68f0
Ibrahim Haizel <ibrahim.haizel@communities.gov.uk>
2025-04-11 23:37:03 +0100

    add examples

--- src/wrappers/components/ui/related-content/Examples.svelte
+++ src/wrappers/components/ui/related-content/Examples.svelte
@@ -1,17 +1,109 @@
-<script>
+<script lang="ts">
   import { AccordionItem, Accordion } from "flowbite-svelte";
 
   import CodeBlock from "$lib/package-wrapping/CodeBlock.svelte";
   import * as codeBlocks from "./codeBlocks.js";
 
-import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  // Data for examples (adapted from GOV.UK docs)
+  const orderedRelatedItemsData = [
+    { title: "Find an apprenticeship", base_path: "/apply-apprenticeship" },
+    {
+      title: "Training and study at work",
+      base_path: "/training-study-work-your-rights",
+    },
+    {
+      title: "Careers helpline for teenagers",
+      base_path: "/careers-helpline-for-teenagers",
+    },
+  ];
+  const mainstreamData = [
+    {
+      title: "Driving licences",
+      base_path: "/browse/driving/driving-licences",
+    },
+    {
+      title: "Driving tests and learning to drive or ride",
+      base_path: "/browse/driving/learning-to-drive",
+    },
+  ];
+  const taxonsData = [
+    {
+      title: "Driving instruction and driving lessons",
+      base_path: "/transport/driving-instruction-and-driving-lessons",
+    },
+  ];
+  const collectionsData = [
+    {
+      title: "Recruit an apprentice (formerly apprenticeship vacancies)",
+      base_path: "/government/collections/apprenticeship-vacancies",
+    },
+    {
+      title: "The future of jobs and skills",
+      base_path: "/government/collections/the-future-of-jobs-and-skills",
+    },
+  ];
+  const topicalEventsData = [
+    {
+      title: "UK-China High-Level People to People Dialogue 2017",
+      base_path:
+        "/government/topical-events/uk-china-high-level-people-to-people-dialogue-2017",
+    },
+  ];
+  const worldLocationsData = [
+    { title: "South Sudan", base_path: "/world/south-sudan/news" },
+    { title: "USA", base_path: "/world/usa/news" },
+  ];
+  const externalLinksData = [
+    {
+      url: "http://media.slc.co.uk/sfe/1718/ft/sfe_terms_and_conditions_guide_1718_d.pdf",
+      title: "Student loans: terms and conditions 2017 to 2018 (PDF, 136KB)",
+    },
+    {
+      url: "https://www.thestudentroom.co.uk/content.php?r=5967-Repaying-your-student-loan",
+      title: "The Student Room: repaying your student loan",
+    },
+  ];
+  const statsData = [
+    {
+      title: "International road fuel prices",
+      base_path:
+        "/government/statistical-data-sets/comparisons-of-industrial-and-domestic-energy-prices-monthly-figures",
+    },
+    {
+      title: "Weekly road fuel prices",
+      base_path:
+        "/government/statistical-data-sets/oil-and-petroleum-products-weekly-statistics",
+    },
+  ];
+  const contactsData = [{ title: "Pest Control", base_path: "/pest-control" }];
 
   let accordionSnippetSections = [
     {
       id: "1",
-      heading: "1. Example 1 - describe the use case here",
+      heading: "1. Default (Ordered Related Items)",
       content: Example1,
     },
+    {
+      id: "2",
+      heading: "2. Explore the Topic (Mainstream Browse Pages)",
+      content: Example2,
+    },
+    { id: "3", heading: "3. Explore the Topic (Taxons)", content: Example3 },
+    {
+      id: "4",
+      heading: "4. Explore the Topic (Mainstream Precedence)",
+      content: Example4,
+    },
+    { id: "5", heading: "5. Collections", content: Example5 },
+    { id: "6", heading: "6. Topical Events", content: Example6 },
+    { id: "7", heading: "7. World Locations", content: Example7 },
+    { id: "8", heading: "8. External Links", content: Example8 },
+    { id: "9", heading: "9. Statistical Data Sets", content: Example9 },
+    { id: "10", heading: "10. Other Contacts", content: Example10 },
+    { id: "11", heading: "11. All Types Combined", content: Example11 },
+    { id: "12", heading: "12. Disable GA4 Tracking", content: Example12 },
   ];
 </script>
 
@@ -36,33 +128,101 @@ import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
 </div>
 
 {#snippet Example1()}
-  <div class="p-5 bg-white">
-    <Template
-      componentNameProp="Example 1"
-      checkboxProp={true}
-      dropdownProp="Dragonfruit"
-      jsObjectProp={[
-        {
-          name: "Borussia Dortmund",
-          country: "Germany",
-          color: "#fdff7d",
-        },
-        { name: "Liverpool FC", country: "UK", color: "#f59fad" },
-        {
-          name: "SSC Napoli",
-          country: "Italy",
-          color: "#69bfff",
-        },
-        {
-          name: "S.L. Benfica",
-          country: "Portugal",
-          color: "#ff8c96",
-        },
-      ]}
-      functionProp={function () {
-        window.alert(`Example 1 functionProp has been triggered.`);
-      }}
-    ></Template>
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent orderedRelatedItems={orderedRelatedItemsData} />
   </div>
   <CodeBlock code={codeBlocks.codeBlock1} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example2()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent mainstreamBrowsePages={mainstreamData} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock2} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example3()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent taxons={taxonsData} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock3} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example4()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent
+      mainstreamBrowsePages={mainstreamData}
+      taxons={taxonsData}
+    />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock4} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example5()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent documentCollections={collectionsData} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock5} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example6()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent topicalEvents={topicalEventsData} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock6} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example7()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent worldLocations={worldLocationsData} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock7} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example8()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent externalRelatedLinks={externalLinksData} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock8} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example9()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent relatedStatisticalDataSets={statsData} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock9} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example10()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent otherContacts={contactsData} />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock10} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example11()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent
+      orderedRelatedItems={orderedRelatedItemsData}
+      documentCollections={collectionsData}
+      mainstreamBrowsePages={mainstreamData}
+      taxons={taxonsData}
+      topicalEvents={topicalEventsData}
+      worldLocations={worldLocationsData}
+      relatedStatisticalDataSets={statsData}
+      externalRelatedLinks={externalLinksData}
+      otherContacts={contactsData}
+    />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock11} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet Example12()}
+  <div class="p-5 bg-white max-w-xs">
+    <RelatedContent
+      orderedRelatedItems={orderedRelatedItemsData}
+      disableGa4={true}
+    />
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock12} language="svelte"></CodeBlock>
 {/snippet}
--- src/wrappers/components/ui/related-content/codeBlocks.js
+++ src/wrappers/components/ui/related-content/codeBlocks.js
@@ -1,32 +1,177 @@
+// Adapted from https://components.publishing.service.gov.uk/component-guide/related_navigation
+
 export const codeBlock1 = `
 <script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
 
-  import Template from "$lib/package-wrapping/Template.svelte";
+  const items = [
+    { title: "Find an apprenticeship", base_path: "/apply-apprenticeship" },
+    { title: "Training and study at work", base_path: "/training-study-work-your-rights" },
+    { title: "Careers helpline for teenagers", base_path: "/careers-helpline-for-teenagers" },
+  ];
+</script>
+
+<RelatedContent orderedRelatedItems={items} />
+`;
+
+export const codeBlock2 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { title: "Driving licences", base_path: "/browse/driving/driving-licences" },
+    { title: "Driving tests and learning to drive or ride", base_path: "/browse/driving/learning-to-drive" },
+  ];
+</script>
+
+<RelatedContent mainstreamBrowsePages={items} />
+`;
+
+export const codeBlock3 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { title: "Driving instruction and driving lessons", base_path: "/transport/driving-instruction-and-driving-lessons" },
+  ];
+</script>
+
+<RelatedContent taxons={items} />
+`;
+
+export const codeBlock4 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const mainstream = [
+    { title: "Driving licences", base_path: "/browse/driving/driving-licences" },
+    { title: "Driving tests and learning to drive or ride", base_path: "/browse/driving/learning-to-drive" },
+  ];
+  const taxons = [
+    { title: "Driving instruction and driving lessons", base_path: "/transport/driving-instruction-and-driving-lessons" },
+  ];
+</script>
+
+<!-- Mainstream browse pages take precedence -->
+<RelatedContent mainstreamBrowsePages={mainstream} taxons={taxons} />
+`;
+
+export const codeBlock5 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { title: "Recruit an apprentice (formerly apprenticeship vacancies)", base_path: "/government/collections/apprenticeship-vacancies" },
+    { title: "The future of jobs and skills", base_path: "/government/collections/the-future-of-jobs-and-skills" },
+  ];
+</script>
+
+<RelatedContent documentCollections={items} />
+`;
+
+export const codeBlock6 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { title: "UK-China High-Level People to People Dialogue 2017", base_path: "/government/topical-events/uk-china-high-level-people-to-people-dialogue-2017" },
+  ];
+</script>
+
+<RelatedContent topicalEvents={items} />
+`;
+
+export const codeBlock7 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { title: "South Sudan", base_path: "/world/south-sudan/news" },
+    { title: "USA", base_path: "/world/usa/news" },
+  ];
+</script>
+
+<RelatedContent worldLocations={items} />
+`;
+
+export const codeBlock8 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { url: "http://media.slc.co.uk/sfe/1718/ft/sfe_terms_and_conditions_guide_1718_d.pdf", title: "Student loans: terms and conditions 2017 to 2018 (PDF, 136KB)" },
+    { url: "https://www.thestudentroom.co.uk/content.php?r=5967-Repaying-your-student-loan", title: "The Student Room: repaying your student loan" },
+  ];
+</script>
+
+<RelatedContent externalRelatedLinks={items} />
+`;
+
+export const codeBlock9 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { title: "International road fuel prices", base_path: "/government/statistical-data-sets/comparisons-of-industrial-and-domestic-energy-prices-monthly-figures" },
+    { title: "Weekly road fuel prices", base_path: "/government/statistical-data-sets/oil-and-petroleum-products-weekly-statistics" },
+  ];
+</script>
+
+<RelatedContent relatedStatisticalDataSets={items} />
+`;
+
+export const codeBlock10 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { title: "Pest Control", base_path: "/pest-control" },
+  ];
+</script>
+
+<RelatedContent otherContacts={items} />
+`;
+
+export const codeBlock11 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  // Data defined for each section (as in previous examples)
+  const orderedRelatedItemsData = [{ title: "Find an apprenticeship", base_path: "/apply-apprenticeship" }, /* ... */];
+  const collectionsData = [{ title: "Recruit an apprentice...", base_path: "..." }, /* ... */];
+  const mainstreamData = [{ title: "Driving licences", base_path: "..." }, /* ... */];
+  const taxonsData = [{ title: "Driving instruction...", base_path: "..." }];
+  const topicalEventsData = [{ title: "UK-China High-Level...", base_path: "..." }];
+  const worldLocationsData = [{ title: "South Sudan", base_path: "..." }, /* ... */];
+  const statsData = [{ title: "International road fuel prices", base_path: "..." }, /* ... */];
+  const externalLinksData = [{ url: "...", title: "Student loans..." }, /* ... */];
+  const contactsData = [{ title: "Pest Control", base_path: "..." }];
 
 </script>
 
-<Template
-checkboxProp={true}
-dropdownProp="Dragonfruit"
-jsObjectProp={[
-  {
-    name: "Borussia Dortmund",
-    country: "Germany",
-    color: "#fdff7d",
-  },
-  { name: "Liverpool FC", country: "UK", color: "#f59fad" },
-  {
-    name: "SSC Napoli",
-    country: "Italy",
-    color: "#69bfff",
-  },
-  {
-    name: "S.L. Benfica",
-    country: "Portugal",
-    color: "#ff8c96",
-  },
-]}
-functionProp={function (event, pokemon) {
-  window.alert("Example 1 functionProp has been triggered.");
-}}
-></Template>`;
+<RelatedContent
+  orderedRelatedItems={orderedRelatedItemsData}
+  documentCollections={collectionsData}
+  mainstreamBrowsePages={mainstreamData}
+  taxons={taxonsData}
+  topicalEvents={topicalEventsData}
+  worldLocations={worldLocationsData}
+  relatedStatisticalDataSets={statsData}
+  externalRelatedLinks={externalLinksData}
+  otherContacts={contactsData}
+/>
+`;
+
+export const codeBlock12 = `
+<script>
+  import RelatedContent from "$lib/components/ui/RelatedContent.svelte";
+
+  const items = [
+    { title: "Find an apprenticeship", base_path: "/apply-apprenticeship" },
+    { title: "Training and study at work", base_path: "/training-study-work-your-rights" },
+    { title: "Careers helpline for teenagers", base_path: "/careers-helpline-for-teenagers" },
+  ];
+</script>
+
+<RelatedContent orderedRelatedItems={items} disableGa4={true} />
+`;

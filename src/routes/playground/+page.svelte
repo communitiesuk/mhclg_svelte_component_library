72bcdc3ad685d2bfb55219af34aabbbbd19c08b9
mileshinch <miles.hinchliffe@levellingup.gov.uk>
2025-04-15 15:01:46 +0100

    Add playground page to test button tracker

---
+++ src/routes/playground/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Button from "$lib/components/ui/Button.svelte";
+
+  let buttonStatus = $state(false);
+
+  // function trackClick() {
+  //     buttonStatus = true
+  // }
+</script>
+
+<Button
+  textContent="Click me"
+  buttonType="default"
+  trackClick={() => (buttonStatus = true)}
+></Button>
+
+<div>
+  {buttonStatus}
+</div>

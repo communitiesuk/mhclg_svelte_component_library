34dd0f5d147d626125153dbbe4eac953a98d7ac0
Ibrahim Haizel <ibrahim.haizel@communities.gov.uk>
2025-05-02 17:13:27 +0100

    cleaned css file names

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // this file is auto-generated — do not edit by hand
-import "$lib/components_base.css";
+import "$lib/components_global.css";
 
 export { default as InsetText } from './components/content/InsetText.svelte';
 export { default as WarningText } from './components/content/WarningText.svelte';

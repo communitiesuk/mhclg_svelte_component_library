2cfbbf47d65cd6836d942260451e7058d3c13778
Duncan Bradley <duncan.bradley@communities.gov.uk>
2025-05-06 16:18:26 +0100

    update progress

--- src/wrappers/components/data-vis/line-chart/LinesWrapper.svelte
+++ src/wrappers/components/data-vis/line-chart/LinesWrapper.svelte
@@ -90,7 +90,7 @@
    * ?  Tested - The component's use within products or prototyping (i.e. in a real-use example, using real props) has been tested and approved.
    */
   let statusObject = {
-    progress: "To be developed",
+    progress: "In Progress",
     statusRows: [
       {
         obj: { Accessible: false, Responsive: false, "Prog. enhanced": false },

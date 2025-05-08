90d0c2be11ba86091838d8540895aafd56c8eb0f
cjonesof <camilla.jones1@communities.gov.uk>
2025-05-08 14:27:07 +0100

    Created a data join file for map

--- src/lib/components/data-vis/map/Map.svelte
+++ src/lib/components/data-vis/map/Map.svelte
@@ -26,6 +26,8 @@
   } from "./mapUtils.js";
   import NonStandardControls from "./NonStandardControls.svelte";
 
+  import { joinData } from "./dataJoin.ts";
+
   let {
     data,
     cooperativeGestures,
@@ -125,45 +127,9 @@
     }),
   );
 
-  //Joining the data to the GeoJSON
-  let obj2Map = $derived(
-    dataWithColor?.reduce((map, item) => {
-      map[item.areaCode] = item; // Use 'areaCode' as the key for the second map
-      return map;
-    }, {}),
-  );
+  let merged = $derived(joinData(filteredGeoJsonData, dataWithColor));
 
-  let obj1Map = $derived(
-    filteredGeoJsonData.features.reduce((map, item) => {
-      map[item.properties.areacd] = item; // Use 'areacd' from properties as the key for the first map
-      return map;
-    }, {}),
-  );
-
-  // Merge both datasets
-  let merged = $derived({
-    type: "FeatureCollection",
-    features: filteredGeoJsonData.features.map((item1) => {
-      // Get the matching item from obj2Map based on the areaCode
-      const match = obj2Map[item1.properties.areacd];
-
-      // If a match exists, merge the 'metric' and 'color' into the 'properties' of item1
-      if (match) {
-        return {
-          ...item1, // Keep all properties of the feature
-          properties: {
-            ...item1.properties, // Keep the existing properties (like areaCode, etc.)
-            metric: +match?.metric, // Add metric from the match
-            color: match?.color, // Add color from the match
-          },
-        };
-      }
-
-      // If no match, just return the feature as is
-      return item1;
-    }),
-    crs: { properties: { name: "EPSG:4326" }, type: "name" },
-  });
+  $inspect(merged);
 
   let hoveredArea = $state();
   let hoveredAreaData = $state();

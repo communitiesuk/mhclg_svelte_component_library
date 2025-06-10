3c3b8221f4bb4a0f5699710e8bc6fad51a9a76b5
Andrew Hillman <63984614+ajhillman-ddj@users.noreply.github.com>
2025-06-10 13:52:34 +0100

    Build gh-pages into docs folder

--- svelte.config.js
+++ svelte.config.js
@@ -29,7 +29,7 @@ const config = {
       },
     },
     paths: {
-      base: process.env.NODE_ENV === "production" ? "/gh-pages" : "",
+      base: process.env.NODE_ENV === "production" ? "/gh-pages-build" : "",
     },
     files: {
       assets: "static",

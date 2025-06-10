76c580d488701131ef712ee02f8973da192254dc
Andrew Hillman <63984614+ajhillman-ddj@users.noreply.github.com>
2025-06-10 13:49:37 +0100

    Remove netlify folder

--- .netlify/server/nodes/7.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 7;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/components/ui/accordion/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/7.Uf-fbAmT.js","_app/immutable/chunks/CKb67zNv.js","_app/immutable/chunks/BqQgSr2I.js","_app/immutable/chunks/BAnyEor3.js","_app/immutable/chunks/DOpM9Ngp.js","_app/immutable/chunks/BupwT6iW.js","_app/immutable/chunks/mfAo6MD8.js","_app/immutable/chunks/B6T8vOCz.js","_app/immutable/chunks/BgMzVouQ.js","_app/immutable/chunks/NWAaYpEX.js","_app/immutable/chunks/D_z4nuJ4.js","_app/immutable/chunks/BmHjbJce.js","_app/immutable/chunks/DSCCKllV.js","_app/immutable/chunks/CQyCnfGD.js","_app/immutable/chunks/D_YaxJbW.js","_app/immutable/chunks/DSRkmSfG.js","_app/immutable/chunks/CbyIEU9a.js","_app/immutable/chunks/D_tBpuM-.js","_app/immutable/chunks/CBsNxnYr.js","_app/immutable/chunks/vSpGRw5b.js","_app/immutable/chunks/BPTxYl1-.js","_app/immutable/chunks/tflgWAN7.js","_app/immutable/chunks/mc4jfO9x.js","_app/immutable/chunks/C1FmrZbK.js"];
-export const stylesheets = ["_app/immutable/assets/7.Bbxfmn8I.css","_app/immutable/assets/trackVisibleParameters.D8BiI8vn.css"];
-export const fonts = [];

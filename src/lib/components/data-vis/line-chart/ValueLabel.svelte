ff86afd7ef44610c90a1200f9839e57f2a0ac1d9
Duncan Bradley <duncan.bradley@communities.gov.uk>
2025-06-25 14:47:53 +0100

    always position tooltip above marker

--- src/lib/components/data-vis/line-chart/ValueLabel.svelte
+++ src/lib/components/data-vis/line-chart/ValueLabel.svelte
@@ -17,25 +17,26 @@
   let horizontalPadding = $derived(verticalPadding * 2);
 </script>
 
-{#if tooltipSnippet === undefined}
-  <div
-    style="position:absolute;
+<div
+  style="position:absolute;
   top: {markerRect?.y - (textDimensions?.height ?? 0) - 15}px;
 left: {markerRect?.x +
-      (markerRect?.width ?? 0) / 2 -
-      (textDimensions?.width ?? 0) / 2}px;
+    (markerRect?.width ?? 0) / 2 -
+    (textDimensions?.width ?? 0) / 2}px;
   pointer-events: none;
-  background-color: {labelColor};
+  "
+>
+  {#if tooltipSnippet === undefined}
+    <div
+      style="background-color: {labelColor};
   padding: 5px;
-  border-radius: 5px;
-  height={textDimensions?.height ?? 0}
-  width={textDimensions?.width ?? 0}"
-  >
-    <div bind:contentRect={textDimensions}>{activeMarkerId.y}</div>
-  </div>
-{:else}<div
-    style="position: absolute; top: {markerRect?.y}px; left: {markerRect?.x}px;"
-  >
-    {@render tooltipSnippet(activeMarkerId)}
-  </div>
-{/if}
+  border-radius: 5px;"
+    >
+      <div bind:contentRect={textDimensions}>{activeMarkerId.y}</div>
+    </div>
+  {:else}
+    <div bind:contentRect={textDimensions}>
+      {@render tooltipSnippet(activeMarkerId)}
+    </div>
+  {/if}
+</div>

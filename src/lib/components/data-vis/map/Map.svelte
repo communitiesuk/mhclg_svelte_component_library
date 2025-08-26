70a6793684b66e5ccdd3f31a7b154175330f9c9d
Andrew Hillman <63984614+ajhillman-ddj@users.noreply.github.com>
2025-08-26 10:48:38 +0100

    Legend logic reordering

--- src/lib/components/data-vis/map/Map.svelte
+++ src/lib/components/data-vis/map/Map.svelte
@@ -100,7 +100,7 @@
     onstyleload,
     onstyledata,
     onidle,
-    showLegend = true,
+    showLegend = false,
     legendSnippet = undefined,
     countries = ["england", "scotland"],
     areaCode = "",
@@ -692,25 +692,23 @@
   </MapLibre>
 </div>
 
-{#if legendSnippet && showLegend}
-  {#if typeof legendSnippet === "string"}
-    <div class="legend">
-      {@html legendSnippet}
-    </div>
-  {:else}
-    <svelte:component this={legendSnippet} />
-  {/if}
-{:else if showLegend}
-  <!-- Default legend -->
+{#if showLegend}
   <div class="legend">
-    {#each legendItems as item}
-      <div class="legend-item">
-        <div class="legend-color" style="background-color: {item.color};"></div>
-        <span>{item.label}</span>
-      </div>
-    {/each}
+    {#if legendSnippet}
+      {@render legendSnippet()}
+    {:else}
+      {#each legendItems as item}
+        <div class="legend-item">
+          <div
+            class="legend-color"
+            style="background-color: {item.color};"
+          ></div>
+          <span>{item.label}</span>
+        </div>
+      {/each}
+    {/if}
   </div>
-{:else}{/if}
+{/if}
 
 <style>
   :global(.maplibregl-ctrl-group button.reset-button) {

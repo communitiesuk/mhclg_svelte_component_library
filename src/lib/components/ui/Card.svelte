ff6590ee1cc38b8ca4e46b192755ac9cdffc1bd0
Miles Hinchliffe <miles.hinchliffe@levellingup.gov.uk>
2025-09-05 12:32:55 +0100

    Build card suitable for IMD link cards on home and data download pages

--- src/lib/components/ui/Card.svelte
+++ src/lib/components/ui/Card.svelte
@@ -1,105 +1,58 @@
 <script>
   let {
-    componentNameProp = undefined,
-    textProp = undefined,
-    numberProp = undefined,
-    checkboxProp = false,
-    dropdownProp = undefined,
-    radioProp = undefined,
-    jsObjectProp = [],
-    functionProp = undefined,
-    textContent = undefined,
+    linkCard = true,
+    linkText = undefined,
+    href = undefined,
+    chevronSrc = "/assets/images/ons-icon-chevron-small.svg",
+    bodyText = undefined,
+    callToActionBackgroundColor = "white",
+    bodyBackgroundColor = "#FBFCFD",
+    bottomBorderColor = "#c3d9e9",
   } = $props();
 </script>
 
-{#snippet propNameAndValue(marginTW, paddingTW, text)}
-  <span class="bg-slate-100 inline-block italic {marginTW} {paddingTW} rounded"
-    >{text}</span
+<div class="card" style="border-bottom: 1px solid {bottomBorderColor};">
+  <div
+    class="call-to-action"
+    style="background-color: {callToActionBackgroundColor};"
   >
-{/snippet}
-
-<div>
-  {textContent}
-</div>
-
-<div class="p-4">
-  <h4>{componentNameProp} component</h4>
-
-  {#each [{ name: "textProp", prop: textProp }, { name: "numberProp", prop: numberProp }] as output}
-    {#if output.prop != undefined}
-      <div>
-        <p class="mb-0">
-          The <span class="font-bold">{output.name}</span> value being passed to
-          the component is:
-          {#if output.name === "numberProp"}
-            {@render propNameAndValue("m-2", "py-2 pl-2 pr-3", numberProp)}
-          {/if}
-        </p>
-        {#if output.name === "textProp"}
-          {@render propNameAndValue(
-            "ml-6 mt-4",
-            "py-2 pl-2 pr-3",
-            "'" + output.prop + "'",
-          )}
-        {/if}
+    {#if linkCard}
+      <div
+        class="link"
+        style="display: flex; align-items: center; justify-content: space-between;"
+      >
+        <span class="govuk-heading-m">
+          <a {href} class="govuk-link">{linkText}</a>
+        </span>
+        <img src={chevronSrc} alt="Chevron icon" class="chevron" />
       </div>
+    {:else}
+      <div>A different call to action</div>
     {/if}
-  {/each}
-
-  <div class="grid grid-cols-3 gap-4 mt-6">
-    {#each [{ name: "checkboxProp", prop: checkboxProp }, { name: "dropdownProp", prop: dropdownProp }, { name: "radioProp", prop: radioProp }] as card}
-      {#if card.prop != undefined}
-        <div class="p-2">
-          <p class="my-2">
-            <span class="font-bold">{card.name}</span> is set to:
-          </p>
-          <p class="p-2 inline-block italic bg-slate-100 rounded">
-            {card.prop}
-          </p>
-        </div>
-      {/if}
-    {/each}
   </div>
 
-  <div class="mt-10">
-    <p class="font-bold">jsObjectProp:</p>
-    <p>
-      Click on a card to trigger a call of the functionProp, which will write to
-      the console.
-    </p>
-    <div class="grid grid-cols-2 gap-4 mt-6 cursor-pointer">
-      {#each jsObjectProp as pokemon}
-        <button
-          data-id={pokemon.name}
-          data-role="pokemon-card"
-          class="rounded p-2 grid grid-cols-[auto_1fr] gap-2"
-          style="background-color: {pokemon.color};"
-          onclick={(event) => functionProp(event, pokemon)}
-        >
-          {#each Object.keys(pokemon) as key}
-            {#if key != "color"}
-              <p class="m-0 p-0">{key}:</p>
-              <p class="m-0 p-0">{pokemon[key]}</p>
-            {/if}
-          {/each}
-        </button>
-      {/each}
-    </div>
+  <div class="body" style="background-color: {bodyBackgroundColor};">
+    {bodyText}
   </div>
 </div>
 
 <style>
-  [data-role="pokemon-card"] {
-    opacity: 0.6;
-    border: 2px solid transparent;
+  .call-to-action {
+    padding: 15px 20px;
   }
 
-  [data-role="pokemon-card"]:hover {
-    opacity: 0.8;
-    border: 2px solid;
+  .chevron {
+    width: 20px;
+    height: 20px;
   }
-  [data-role="pokemon-card"]:focus {
-    opacity: 1;
-    border: 2px solid;
+
+  .body {
+    padding: 20px 15px;
+  }
+
+  .govuk-heading-m {
+    /* overwriting margin and line-height to make link centered vertically */
+    margin: 0;
+    line-height: 1;
   }
 </style>

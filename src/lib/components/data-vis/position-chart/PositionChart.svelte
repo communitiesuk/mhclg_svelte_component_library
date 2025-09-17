3f4818645082e5481e510f6510c18d2564ec85b9
Duncan Bradley <duncan.bradley@communities.gov.uk>
2025-09-17 15:23:45 +0100

    add annotation and info icons

--- src/lib/components/data-vis/position-chart/PositionChart.svelte
+++ src/lib/components/data-vis/position-chart/PositionChart.svelte
@@ -26,6 +26,7 @@
     interactiveMarkers = true,
     labelText = "hello world",
     tooltipContent = undefined,
+    topLabel = true,
     container = undefined,
     yFunction = () => 10,
     x = 0,
@@ -33,6 +34,9 @@
     currentMousePosition = undefined,
     markerRect = undefined,
     tooltipSnippet = undefined,
+    onClickIcon = (label) => {
+      openLabel = openLabel === label ? null : label;
+    },
     onClickMarker = (event, marker, markerId) => {
       activeMarkerId = marker;
       currentMousePosition = [event.screenX, event.screenY];
@@ -68,6 +72,8 @@
   // base defaults that apply to every chart
   const baseChart = { label, chartHeight, min, max, showAxis };
 
+  let topWidth = $state();
+
   let allDataNormalized = $derived(
     allData.map((chart) => {
       // create a container for tiered rowData
@@ -100,16 +106,35 @@
     }),
   );
 
+  let openLabel = $state();
+  let annotation = $state(true);
+
+  let annotationArrowPosition = $derived(
+    Object.values(allDataNormalized[0].rowData)[0][0].value,
+  );
+
+  let annotationText = $state("Bournemouth, Christchurch and Poole 008A");
+
   let showLabel = $derived(
     allDataNormalized.some((obj) => obj.label !== undefined),
   );
-
+  let showIcon = $state(true);
   let numberOfPositionCharts = $derived(allDataNormalized.length);
-  let gridTemplateColumns = $derived(showLabel ? "auto 1fr" : "1fr");
-  let gridTemplateRows = $derived(
-    `repeat(${showAxis ? numberOfPositionCharts + 2 : numberOfPositionCharts}, auto)`,
+  let gridTemplateColumns = $derived(
+    showIcon && showLabel
+      ? "auto auto 1fr"
+      : showIcon
+        ? "auto 1fr"
+        : showLabel
+          ? "auto 1fr"
+          : "1fr",
   );
 
+  let gridTemplateRows = $derived(() => {
+    let rows = showAxis ? numberOfPositionCharts + 2 : numberOfPositionCharts;
+    if (open) rows += 1;
+    return `repeat(${rows}, auto)`;
+  });
   const range = $derived(Array.from({ length: nSegments }, (_, i) => i));
 
   function interpolateColors(hex1, hex2, steps, hexMid = null) {
@@ -206,6 +231,61 @@
   // ];
 </script>
 
+{#if annotation}
+  <div bind:clientWidth={topWidth}>
+    <svg width={topWidth} height="88">
+      <!-- {#if !activeMarkerId} -->
+      <g>
+        <!-- Example text inside SVG -->
+        <text
+          id="label"
+          x="0"
+          y="50"
+          fill="purple"
+          font-size="20"
+          stroke-width="3"
+          opacity={typeof activeMarkerId !== "undefined" && activeMarkerId
+            ? 0.2
+            : 1}
+        >
+          {annotationText}
+        </text>
+      </g>
+      <defs>
+        <!-- Reusable arrow marker -->
+        <marker
+          id="arrow-down"
+          markerWidth="10"
+          markerHeight="10"
+          refX="3"
+          refY="3"
+          orient="auto"
+          markerUnits="strokeWidth"
+          opacity={typeof activeMarkerId !== "undefined" && activeMarkerId
+            ? 0.2
+            : 1}
+        >
+          <path d="M 0 0 L 6 3 L 0 6 z" fill="purple"></path>
+        </marker>
+      </defs>
+      <path
+        d="M 4 55 v 10 h {xFunction(min, max)(annotationArrowPosition) +
+          markerRadius -
+          4 +
+          (topWidth - chartWidth)}  v 15"
+        fill="none"
+        stroke="purple"
+        stroke-width="2"
+        marker-end="url(#arrow-down)"
+        opacity={typeof activeMarkerId !== "undefined" && activeMarkerId
+          ? 0.2
+          : 1}
+      ></path>
+      <!-- {/if} -->
+    </svg>
+  </div>
+{/if}
+
 <div
   class="grid-container"
   bind:this={container}
@@ -216,24 +296,14 @@
   "
 >
   {#each allDataNormalized as positionChart, i}
-    <div><p></p></div>
-    <div>
-      <svg width={chartWidth} height="70">
-        <g>
-          <text x="10" y="50" fill="black" font-size="20">
-            Bournemouth, Christchurch and Poole 008A
-          </text>
-          <path
-            d="M 10,0 L 10,-10 L 20,-10 L 20,-20"
-            fill="none"
-            stroke="black"
-            stroke-width="1"
-          ></path>
-        </g>
-      </svg>
-    </div>
     {#if showLabel}
       <p class="label">{positionChart.label}</p>
+    {/if}
+    {#if showIcon}
+      <button
+        id="info-{positionChart.label}"
+        onclick={() => onClickIcon(positionChart.label)}>ⓘ</button
+      >
     {/if}
     <div
       class="chart"
@@ -241,16 +311,6 @@
       bind:clientWidth={chartWidth}
     >
       <svg width={chartWidth} height={positionChart.chartHeight}>
-        <svg
-          ><circle
-            r={markerRadius}
-            cx="80"
-            cy="80"
-            fill="black"
-            stroke="white"
-            opacity="1"
-          ></circle></svg
-        >
         {#each range as number}
           <g
             transform="translate({markerRadius +
@@ -312,10 +372,21 @@
         {/each}
       </svg>
     </div>
+    {#if openLabel === positionChart.label}
+      <div style="grid-column:1 / -1; background-color:lightgrey; padding:10px">
+        <p>
+          Description for {positionChart.label}
+        </p>
+      </div>
+    {/if}
   {/each}
-
-  {#if showAxis === true}
-    <div class="empty"></div>
+  {#if showAxis}
+    {#if showIcon}
+      <div class="empty"></div>
+    {/if}
+    {#if showLabel}
+      <div class="empty"></div>
+    {/if}
     <div class="axis">
       <PositionChartAxis {markerRadius} {barWidth}></PositionChartAxis>
     </div>

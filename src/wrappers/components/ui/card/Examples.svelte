e78644d113af45c70f2e094fc7ba326feb466a0e
Miles Hinchliffe <miles.hinchliffe@levellingup.gov.uk>
2025-12-16 12:43:31 +0000

    Add Card examples

--- src/wrappers/components/ui/card/Examples.svelte
+++ src/wrappers/components/ui/card/Examples.svelte
@@ -7,17 +7,24 @@
   import Card from "$lib/components/ui/Card.svelte";
 
   import PositionChart from "$lib/components/data-vis/position-chart/PositionChart.svelte";
+  import PostcodeOrAreaSearch from "$lib/components/ui/PostcodeOrAreaSearch.svelte";
 
   let accordionSnippetSections = [
     {
       id: "1",
-      heading: "1. Example 1 - simple card with heading link, and body text",
+      heading: "1. Example 1 - a simple card with heading link, and body text",
       content: Example1,
     },
     {
       id: "2",
-      heading: "2. Example 2 - card with a chart",
+      heading: "2. Example 2 - a card with a chart in the body",
       content: Example2,
+    },
+    {
+      id: "3",
+      heading:
+        "3. Example 3 - a card with a search component instead heading link",
+      content: Example3,
     },
   ];
 </script>
@@ -66,5 +73,43 @@
 {/snippet}
 
 {#snippet cardBottomSnippet()}
-  <PositionChart></PositionChart>
+  <PositionChart
+    value="4.5"
+    min="0"
+    max="10"
+    annotation="East Sussex"
+    showAxis={false}
+    chartHeight={30}
+  ></PositionChart>
+  <p class="pt-4 pl-3">East Sussex: around average.</p>
+{/snippet}
+
+{#snippet Example3()}
+  <div class="p-5 bg-white">
+    <Card
+      linkCard={false}
+      onlyTextInBody={true}
+      bodyText="The interaction in the card header is a search component instead of a link."
+      {cardTopSnippet}
+    ></Card>
+  </div>
+  <CodeBlock code={codeBlocks.codeBlock1} language="svelte"></CodeBlock>
+{/snippet}
+
+{#snippet cardTopSnippet()}
+  <PostcodeOrAreaSearch
+    hint=""
+    label_text="Search for a postcode"
+    label_size="m"
+    placeholder="e.g. NG8 5GT"
+    margin_bottom="2"
+    autoFocusSubmitOnSelection={true}
+    hideHint={false}
+    autoselect={false}
+    customPlacesData={[]}
+    customSourceSelector={() => "api"}
+    minLength={3}
+    label_id="postcode-search"
+    wrap_label_in_a_heading={true}
+  ></PostcodeOrAreaSearch>
 {/snippet}

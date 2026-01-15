33fc3a0345c4dc43b6f640823a7c8bedf7442133
Miles Hinchliffe <miles.hinchliffe@levellingup.gov.uk>
2026-01-15 16:45:41 +0000

    Call CardHeader from Card

--- src/lib/components/ui/Card.svelte
+++ src/lib/components/ui/Card.svelte
@@ -1,15 +1,13 @@
 <script>
+  import CardHeader from "./CardHeader.svelte";
+
   let {
     headerIsLink = true,
-    headerText = "Card heading",
-    headerSize = "m",
-    href = undefined,
     onlyTextInBody = true,
     bodyText = "Text in the body of the card",
     headerSnippet = undefined,
     bodySnippet = undefined,
     children = undefined,
-    headerTextColor = "#1D70B8",
     bodyTextColor = "#0B0C0C",
     headerBackgroundColor = "white",
     bodyBackgroundColor = "#FBFCFD",
@@ -22,24 +20,7 @@
 <div class="card">
   <div class="header-div" style="background-color: {headerBackgroundColor};">
     {#if headerIsLink}
-      <h2 class="link-heading govuk-heading-{headerSize}">
-        <a class="govuk-link link" {href} style="color: {headerTextColor}">
-          {headerText}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="10"
-            height="17"
-            viewBox="0 0 10 17"
-            fill="none"
-            aria-hidden="true"
-          >
-            <path
-              d="M6.21622 8.5L0 2.36667L1.89189 0.5L10 8.5L1.89189 16.5L0 14.6333L6.21622 8.5Z"
-              fill={headerTextColor}
-            ></path>
-          </svg>
-        </a>
-      </h2>
+      <CardHeader></CardHeader>
     {:else}
       {@render headerSnippet()}
     {/if}
@@ -71,14 +52,7 @@
   }
 
   .header-div {
-    padding: 15px 20px;
-  }
-
-  .link {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 100%;
+    padding: 20px;
   }
 
   .body-div {
@@ -91,14 +65,5 @@
 
   .body-text {
     color: var(--body-text-color);
-  }
-
-  .govuk-heading-m {
-    margin: 0;
-    line-height: 1;
-  }
-
-  a {
-    color: #1d70b8;
   }
 </style>

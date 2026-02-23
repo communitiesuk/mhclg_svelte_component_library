71b0431694014916a2ef0826ab1b77cf6f83f2a1
Duncan Bradley <duncan.bradley@communities.gov.uk>
2026-02-23 16:54:47 +0000

    add y axis, polarity, annotation text, padding,

--- src/lib/components/data-vis/histogram/Histogram.svelte
+++ src/lib/components/data-vis/histogram/Histogram.svelte
@@ -13,6 +13,10 @@
     thresholds = 20,
     floor = undefined,
     ceiling = undefined,
+    padding = 0,
+    height = 50,
+    polarity = "standard",
+    annotationText = "",
   } = $props();
 
   import { interpolateColors } from "../position-chart/interpolateColors";
@@ -27,9 +31,12 @@
   let domainMax = max ?? Math.max(...dist);
 
   let xTicks = $state([]);
+  let yTicks = $state([]);
 
   let xTickMin = $derived(xTicks.length ? Math.min(...xTicks) : 0);
   let xTickMax = $derived(xTicks.length ? Math.max(...xTicks) : 1);
+  let yTickMin = $derived(yTicks.length ? Math.min(...yTicks) : 0);
+  let yTickMax = $derived(yTicks.length ? Math.max(...yTicks) : 1);
 
   //  min and max at extremes
   //  let histogram = bin().domain([domainMin, domainMax]).thresholds(thresholds);
@@ -51,8 +58,6 @@
 
   let nBins = $derived(bins.length);
 
-  let histHeight = 50;
-
   let xMin = bins[0]?.x0 ?? 0;
   let xMax = bins[bins.length - 1]?.x1 ?? 1;
 
@@ -60,11 +65,19 @@
 
   let containerWidth = $state(100);
 
-  let xScale = (x) =>
-    ((x - domainMin) / (domainMax - domainMin)) * containerWidth;
+  let useRange = $derived(
+    polarity === "standard"
+      ? [0, containerWidth - padding]
+      : [containerWidth - padding, 0],
+  );
+
+  let xScale = $derived(
+    (x) =>
+      ((x - domainMin) / (domainMax - domainMin)) * (containerWidth - padding),
+  );
 
   let yScale = (count) =>
-    (count / Math.max(...bins.map((b) => b.count))) * histHeight;
+    (count / Math.max(...bins.map((b) => b.count))) * height;
 
   let barWidth = bins.length ? xScale(bins[0].x1) - xScale(bins[0].x0) : 0;
 
@@ -88,14 +101,20 @@
   let highlightIndex = $derived(
     highlightValue !== null ? findBinIndex(bins, highlightValue) : -1,
   );
-
-  $inspect({ highlightValue });
-  $inspect({ highlightIndex });
 </script>
 
 {#key containerWidth}
-  <div class="scale-container" bind:clientWidth={containerWidth}>
-    <svg width={containerWidth} height={histHeight * 1.5}>
+  <div
+    class="scale-container"
+    bind:clientWidth={containerWidth}
+    style="height={height * 4}"
+  >
+    <svg
+      class="chart-container"
+      width={containerWidth - padding}
+      height={height * 2}
+      transform="translate({padding / 2},0)"
+    >
       <defs>
         <marker
           id="arrow-down"
@@ -104,6 +123,7 @@
           refX="0"
           refY="3"
           orient="auto"
+          fill="#666666"
           markerUnits="strokeWidth"
         >
           <!-- Simple upward pointing arrow -->
@@ -113,13 +133,25 @@
       <g transform="translate(0,0)">
         <Axis
           bind:ticksArray={xTicks}
-          chartHeight={histHeight}
-          chartWidth={containerWidth * 0.9}
+          chartHeight={height}
+          chartWidth={containerWidth - padding}
           orientation={{ axis: "x", position: "bottom" }}
           domain={[xTickMin, xTickMax]}
-          range={[0, containerWidth * 0.9]}
+          range={useRange}
           values={dist}
           fontSize={14}
+          {floor}
+          {ceiling}
+        ></Axis>
+        <Axis
+          bind:ticksArray={yTicks}
+          chartHeight={height}
+          chartWidth={containerWidth - padding}
+          orientation={{ axis: "y", position: "left" }}
+          domain={[0, yTickMax]}
+          range={[height, 0]}
+          values={dist}
+          fontSize={0}
           {floor}
           {ceiling}
         ></Axis>
@@ -129,47 +161,75 @@
           {#key bin.x0}
             <rect
               x={xScale(bin.x0)}
-              y={histHeight - yScale(bin.count)}
+              y={height - yScale(bin.count)}
               width={xScale(bin.x1) - xScale(bin.x0)}
               height={yScale(bin.count)}
               fill={interpolatedColors[i]}
-              stroke={i === highlightIndex ? "black" : "white"}
               stroke-width={i === highlightIndex ? 0 : 0}
             ></rect>
           {/key}
         {/each}
+        <g transform="translate(0,0)">
+          {#if highlightIndex !== undefined && bins[highlightIndex]}
+            {#key bins[highlightIndex].x0}
+              <rect
+                x={xScale(bins[highlightIndex].x0)}
+                y={height - yScale(bins[highlightIndex].count)}
+                width={xScale(bins[highlightIndex].x1) -
+                  xScale(bins[highlightIndex].x0)}
+                height={yScale(bins[highlightIndex].count)}
+                fill={interpolatedColors[highlightIndex]}
+                stroke="#666666"
+                stroke-width={2}
+              ></rect>
 
-        {#if highlightIndex !== undefined && bins[highlightIndex]}
-          {#key bins[highlightIndex].x0}
-            <rect
-              x={xScale(bins[highlightIndex].x0)}
-              y={histHeight - yScale(bins[highlightIndex].count)}
-              width={xScale(bins[highlightIndex].x1) -
-                xScale(bins[highlightIndex].x0)}
-              height={yScale(bins[highlightIndex].count)}
-              fill={interpolatedColors[highlightIndex]}
-              stroke="black"
-              stroke-width={2}
-            ></rect>
-
-            <line
-              x1={xScale(bins[highlightIndex].x0)}
-              y2={histHeight - yScale(bins[highlightIndex].count) - 15}
-              x2={xScale(bins[highlightIndex].x0)}
-              y1={histHeight - yScale(bins[highlightIndex].count) - 45}
-              stroke="black"
-              stroke-width="2"
-              marker-end="url(#arrow-down)"
-            ></line>
-          {/key}
-        {/if}
+              <line
+                x1={xScale(bins[highlightIndex].x1) -
+                  (xScale(bins[highlightIndex].x1) -
+                    xScale(bins[highlightIndex].x0)) /
+                    2}
+                y2={height - yScale(bins[highlightIndex].count) - 15}
+                x2={xScale(bins[highlightIndex].x1) -
+                  (xScale(bins[highlightIndex].x1) -
+                    xScale(bins[highlightIndex].x0)) /
+                    2}
+                y1={height - yScale(bins[highlightIndex].count) - 30}
+                stroke="#666666"
+                stroke-width="2"
+                marker-end="url(#arrow-down)"
+              ></line>
+              <g
+                transform="translate({xScale(bins[highlightIndex].x0)},{height -
+                  yScale(bins[highlightIndex].count) -
+                  35})"
+              >
+                <text font-size="0.8em" fill="#777777">{annotationText}</text>
+              </g>
+            {/key}
+          {/if}
+        </g>
       </g>
+      <!-- <g transform="translate({highlightIndex < nBins / 2 ? -5 : -5},-2)">
+        <text font-size="0.8em" fill="#777777">↑ Number of areas</text>
+      </g> -->
     </svg>
+    <PositionChartAxis
+      chartWidth={containerWidth}
+      axisLabels={["Below average", "Above average"]}
+      textSize="xs"
+    ></PositionChartAxis>
   </div>
 {/key}
 
-<PositionChartAxis
-  chartWidth={containerWidth}
-  axisLabels={["Below average", "Above average"]}
-  textSize="xs"
-></PositionChartAxis>
+<style>
+  .chart-container {
+  }
+  .scale-container {
+    /* flex: 1 1 auto; */
+  }
+
+  .scale-container svg {
+    overflow: visible;
+    display: block;
+  }
+</style>

82b2a9b3ba38e7650e42370eb3b31ae85d6548f1
Duncan Bradley <duncan.bradley@communities.gov.uk>
2026-03-13 09:38:16 +0000

    control axis visibility with showAxis

--- src/lib/components/data-vis/position-chart/PositionChart.svelte
+++ src/lib/components/data-vis/position-chart/PositionChart.svelte
@@ -341,19 +341,20 @@
             {/if}
           {/each}
         {/each}
-
-        <Axis
-          bind:ticksArray={xTicks}
-          {chartHeight}
-          chartWidth={chartWidth - markerRadius * 2}
-          orientation={{ axis: "x", position: "bottom" }}
-          range={[markerRadius, chartWidth - markerRadius]}
-          domain={[xTickMin, xTickMax]}
-          values={[0, 2, 3, 4]}
-          fontSize={14}
-          floor={min}
-          ceiling={max}
-        ></Axis>
+        {#if showAxis}
+          <Axis
+            bind:ticksArray={xTicks}
+            {chartHeight}
+            chartWidth={chartWidth - markerRadius * 2}
+            orientation={{ axis: "x", position: "bottom" }}
+            range={[markerRadius, chartWidth - markerRadius]}
+            domain={[xTickMin, xTickMax]}
+            values={[0, 2, 3, 4]}
+            fontSize={14}
+            floor={min}
+            ceiling={max}
+          ></Axis>
+        {/if}
       </svg>
     </div>
     {#if moreInfoTogglesArray[i]}

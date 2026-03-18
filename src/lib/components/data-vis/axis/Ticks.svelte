8e3c83dde2d26a3471aedc69bef3f9425afdd85f
Duncan Bradley <duncan.bradley@communities.gov.uk>
2026-03-18 16:54:45 +0000

    set approx position for tick text based on font size

--- src/lib/components/data-vis/axis/Ticks.svelte
+++ src/lib/components/data-vis/axis/Ticks.svelte
@@ -160,7 +160,7 @@
           ? 5
           : orientation.position === 'top'
             ? -10
-            : 23}
+            : fontSize * 1.4}
         )"
         font-size={fontSize}
         text-anchor={orientation.axis === "x"

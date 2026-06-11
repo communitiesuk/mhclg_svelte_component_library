ded3bc801a3fc345103761849b1319f3decfce9d
cjonesof <camilla.jones1@communities.gov.uk>
2026-06-11 17:15:51 +0100

    Added group and summary html render

--- src/lib/components/ui/Details.svelte
+++ src/lib/components/ui/Details.svelte
@@ -1,19 +1,29 @@
-<script>
+<script lang="ts">
   let {
     summaryText,
     detailedText,
     renderStringAsHTML = false,
     noInset = false,
     expanded = $bindable(false),
+    groupName
   } = $props();
 </script>
 
-<details class="govuk-details" bind:open={expanded}>
-  <summary class="govuk-details__summary">
-    <span class="govuk-details__summary-text">{summaryText}</span>
-  </summary>
-  <div class={`govuk-details__text ${noInset === true ? "no-inset" : ""}`}>
-    {#if typeof detailedText === "string"}
+<details
+        class="govuk-details"
+        open={expanded}
+        name={groupName}
+>
+  {#if renderStringAsHTML}
+    <summary class="govuk-details__summary-text">
+      {@html summaryText}
+    </summary>
+  {:else}
+    <summary class="govuk-details__summary-text">{summaryText}</summary>
+  {/if}
+
+  <div class={`govuk-details__text ${noInset === true ? 'no-inset' : ''}`}>
+    {#if typeof detailedText === 'string'}
       {#if renderStringAsHTML}
         {@html detailedText}
       {:else}

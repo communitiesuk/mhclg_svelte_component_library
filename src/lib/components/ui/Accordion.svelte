3fd69be61ca35cd0bab1f032be6d5b845ca9436d
Miles Hinchliffe <miles.hinchliffe@levellingup.gov.uk>
2026-08-20 16:04:09 +0100

    Export function to enable parent to open section

--- src/lib/components/ui/Accordion.svelte
+++ src/lib/components/ui/Accordion.svelte
@@ -42,29 +42,29 @@
 
   // Attempt to ensure that ids are unique by attaching extra characters
   let uniqueSections = $derived(
-          sections.map((section) => {
-            return {
-              ...section,
-              uniqueid:
-                      section.id +
-                      section.heading.slice(1, 3) +
-                      (section?.summary?.slice(0, 2) ?? ""),
-            };
-          }),
+    sections.map((section) => {
+      return {
+        ...section,
+        uniqueid:
+          section.id +
+          section.heading.slice(1, 3) +
+          (section?.summary?.slice(0, 2) ?? ""),
+      };
+    }),
   );
 
   let expandedSections = $derived(
-          new SvelteSet(
-                  uniqueSections
-                          .filter((section) => section.expanded)
-                          .map((section) => section.uniqueid),
-          ),
+    new SvelteSet(
+      uniqueSections
+        .filter((section) => section.expanded)
+        .map((section) => section.uniqueid),
+    ),
   );
 
   let allExpanded = $derived(expandedSections.size === sections.length);
 
   let ariaLiveValue: "polite" | "off" | "assertive" | null | undefined =
-          $state("polite");
+    $state("polite");
 
   function toggleSection(uniqueid: string) {
     if (expandedSections.has(uniqueid)) {
@@ -79,7 +79,7 @@
   function toggleAll() {
     if (!allExpanded) {
       uniqueSections.forEach((section) =>
-              expandedSections.add(section.uniqueid),
+        expandedSections.add(section.uniqueid),
       );
     } else {
       expandedSections.clear();
@@ -113,51 +113,58 @@
     if (rememberIsExpandedState) {
       uniqueSections.forEach((section) => {
         sessionStorage.setItem(
-                section.uniqueid,
-                expandedSections.has(section.uniqueid).toString(),
+          section.uniqueid,
+          expandedSections.has(section.uniqueid).toString(),
         );
       });
     }
   });
+
+  export function openSection(id: string) {
+    const section = uniqueSections.find((s) => s.id === id);
+    if (section) {
+      expandedSections.add(section.uniqueid);
+    }
+  }
 </script>
 
 <div
-        class="govuk-accordion"
-        class:govuk-accordion--download-style={useLightweightAccordionStyles}
-        data-module="govuk-accordion"
-        id="accordion-default"
+  class="govuk-accordion"
+  class:govuk-accordion--download-style={useLightweightAccordionStyles}
+  data-module="govuk-accordion"
+  id="accordion-default"
 >
   <div
-          class="govuk-accordion__controls"
-          hidden={!allSectionToggle ||
+    class="govuk-accordion__controls"
+    hidden={!allSectionToggle ||
       uniqueSections.length < minSectionsAllSectionToggle}
-          style="display: flex; flex-direction: row"
+    style="display: flex; flex-direction: row"
   >
     <button
-            type="button"
-            class="govuk-accordion__show-all"
-            aria-expanded={allExpanded}
-            onclick={toggleAll}
+      type="button"
+      class="govuk-accordion__show-all"
+      aria-expanded={allExpanded}
+      onclick={toggleAll}
     >
       <span class="govuk-accordion__show-all-text">
         {allExpanded ? hideAllSections : showAllSections}
       </span>
       <span
-              class="govuk-accordion-nav__chevron"
-              class:govuk-accordion-nav__chevron--down={!allExpanded}
+        class="govuk-accordion-nav__chevron"
+        class:govuk-accordion-nav__chevron--down={!allExpanded}
       ></span>
     </button>
   </div>
 
   {#snippet headerContent(section, isExpanded)}
     <button
-            type="button"
-            aria-controls="{section.uniqueid}-content"
-            id="{section.uniqueid}-button"
-            class="govuk-accordion__section-button"
-            aria-expanded={isExpanded}
-            onclick={() => toggleSection(section.uniqueid)}
-            aria-label="{section.heading}, {section.summary
+      type="button"
+      aria-controls="{section.uniqueid}-content"
+      id="{section.uniqueid}-button"
+      class="govuk-accordion__section-button"
+      aria-expanded={isExpanded}
+      onclick={() => toggleSection(section.uniqueid)}
+      aria-label="{section.heading}, {section.summary
         ? section.summary + ','
         : ''} {isExpanded ? hideSectionAriaLabel : showSectionAriaLabel}"
     >
@@ -169,7 +176,7 @@
 
       {#if section.summary}
         <span
-                class="govuk-visually-hidden govuk-accordion__section-heading-divider"
+          class="govuk-visually-hidden govuk-accordion__section-heading-divider"
         >
           ,
         </span>
@@ -184,47 +191,48 @@
         <span class="govuk-accordion__section-toggle-focus">
           {#if useCustomSectionIcon}
             <svg
-                    width="30"
-                    height="30"
-                    viewBox="0 0 30 30"
-                    aria-hidden="true"
-                    class="govuk-accordion__custom-icon"
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+              aria-hidden="true"
+              class="govuk-accordion__custom-icon"
             >
               <g
-                      transform="translate({12 + (isExpanded ? 3 : 0)},{15 + (isExpanded ? -3 : 0)}) rotate({isExpanded ? -45 : -135})"
+                transform="translate({12 + (isExpanded ? 3 : 0)},{15 +
+                  (isExpanded ? -3 : 0)}) rotate({isExpanded ? -45 : -135})"
               >
                 {#each [-5, 5] as cxcy}
                   <circle
-                          data-role={isExpanded
+                    data-role={isExpanded
                       ? "button-circle-selected"
                       : "button-circle-unselected"}
-                          cx={cxcy}
-                          cy={cxcy}
+                    cx={cxcy}
+                    cy={cxcy}
                   ></circle>
                 {/each}
 
                 <path
-                        data-role={isExpanded
+                  data-role={isExpanded
                     ? "button-path-selected"
                     : "button-path-unselected"}
-                        d="M -5 -5 l0 9.75 m0.25 0.25 l9.75 0"
+                  d="M -5 -5 l0 9.75 m0.25 0.25 l9.75 0"
                 ></path>
 
                 <rect
-                        data-role={isExpanded
+                  data-role={isExpanded
                     ? "button-rect-selected"
                     : "button-rect-unselected"}
-                        transform="translate(-6, 4)"
-                        width="2"
-                        height="2"
-                        rx="4px"
+                  transform="translate(-6, 4)"
+                  width="2"
+                  height="2"
+                  rx="4px"
                 ></rect>
               </g>
             </svg>
           {:else}
             <span
-                    class="govuk-accordion-nav__chevron"
-                    class:govuk-accordion-nav__chevron--down={!isExpanded}
+              class="govuk-accordion-nav__chevron"
+              class:govuk-accordion-nav__chevron--down={!isExpanded}
             ></span>
           {/if}
 
@@ -239,8 +247,8 @@
   {#each uniqueSections as section}
     {@const isExpanded = expandedSections.has(section.uniqueid)}
     <div
-            class="govuk-accordion__section"
-            class:govuk-accordion__section--expanded={isExpanded}
+      class="govuk-accordion__section"
+      class:govuk-accordion__section--expanded={isExpanded}
     >
       <div class="govuk-accordion__section-header">
         {#if headingLevel.toLowerCase() == "h2"}
@@ -267,12 +275,12 @@
       </div>
 
       <div
-              id="{section.uniqueid}-content"
-              class="govuk-accordion__section-content"
-              aria-live={ariaLiveValue}
-              hidden={!isExpanded}
-              role={uniqueSections.length < 6 ? "region" : ""}
-              aria-labelledby={uniqueSections.length < 6
+        id="{section.uniqueid}-content"
+        class="govuk-accordion__section-content"
+        aria-live={ariaLiveValue}
+        hidden={!isExpanded}
+        role={uniqueSections.length < 6 ? "region" : ""}
+        aria-labelledby={uniqueSections.length < 6
           ? section.uniqueid + "-button"
           : ""}
       >
@@ -295,37 +303,37 @@
   }
 
   [data-role="button-circle-unselected"] {
-    fill: #0B0C0C;
-    stroke: #0B0C0C;
+    fill: #0b0c0c;
+    stroke: #0b0c0c;
     stroke-width: 1.5;
   }
 
   [data-role="button-circle-selected"] {
-    fill: #0B0C0C;
-    stroke: #0B0C0C;
+    fill: #0b0c0c;
+    stroke: #0b0c0c;
     stroke-width: 1.5;
   }
 
   [data-role="button-path-unselected"] {
-    stroke: #0B0C0C;
+    stroke: #0b0c0c;
     stroke-width: 2;
     fill: none;
     stroke-linecap: round;
   }
 
   [data-role="button-path-selected"] {
-    stroke: #0B0C0C;
+    stroke: #0b0c0c;
     stroke-width: 2;
     fill: none;
     stroke-linecap: round;
   }
 
   [data-role="button-rect-unselected"] {
-    fill: #0B0C0C;
+    fill: #0b0c0c;
   }
 
   [data-role="button-rect-selected"] {
-    fill: #0B0C0C;
+    fill: #0b0c0c;
   }
 
   /* Alternative style mode */
@@ -373,7 +381,9 @@
     display: none;
   }
 
-  .govuk-accordion--download-style .govuk-accordion__section-content .govuk-body {
+  .govuk-accordion--download-style
+    .govuk-accordion__section-content
+    .govuk-body {
     padding-top: 0;
     margin-top: 0;
   }
